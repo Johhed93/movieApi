3b8798b6b5941e35267881b42641e34a5bbe1f29
Johan hedlund Bergqvist <johan.bergqvist@gokstadakademiet.no>
2024-08-30 10:50:40 +0200

    remove clg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import ShowMovie from "../components/ShowMovie";
 import "./Home.css"
-const Home = ({movies}) => {
-  const newArray= movies.find((_,index)=>index===0)
-  console.log(newArray)
+const Home = ({movies, favourites,setNewFavourites}) => {
+ 
   return( <div className="home-container">
     {movies.map((movie, index)=>{
-     {return <ShowMovie key={index} movie={movie}/>}
+     {return <ShowMovie key={index} movie={movie} setNewFavourites={setNewFavourites} favourites={favourites}/>}
     })}
   
   </div>
