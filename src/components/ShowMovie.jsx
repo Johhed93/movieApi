93d9b7e9b558e49913ab22d9d5a3bbe14eace81e
Johan hedlund Bergqvist <johan.bergqvist@gokstadakademiet.no>
2024-08-30 13:06:42 +0200

    logic for if person is on movies/name

--- src/components/ShowMovie.jsx
+++ src/components/ShowMovie.jsx
@@ -1,7 +1,7 @@
 import Button from "./buttons/Button";
 import { useState } from "react";
 import "./ShowMovie.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 const ShowMovie = ({ movie, favourites,setNewFavourites }) => {
   const addToFavourites = () => {
     setNewFavourites((prevState) => [...prevState, movie]);
@@ -10,18 +10,21 @@ const ShowMovie = ({ movie, favourites,setNewFavourites }) => {
     const updatedList= favourites.filter(favourite=> favourite.name !== movie.name)
     setNewFavourites(updatedList)
   }
+const location= useParams();
 
   const isInFavourites= favourites.some(favourite=>favourite.name===movie.name)
   return (
     <div className="container">
       <div className="bg-image"></div>
-      <img src={movie.thumb_url} />
+      <img src={movie.thumb_url} className="thumb-nail"/>
       <div className="card">
         <h2>{movie.name}</h2>
         <p>Year: {movie.year}</p>
         <p>Rating: {movie.rating}⭐</p>
         <div className="button-container">
-          <Link className={"btn2"} to={`/Movies/${movie.name}`}>Les mer</Link>
+          {location!==movie.name ? ( <Link className={"btn2"} to={`/Movies/${movie.name}`}>Les mer</Link>) : (<></>)
+          }
+          
           {!isInFavourites ? (<Button text={"Legg till"} classname={"btn1"} func={addToFavourites} />)
           : 
           (<Button text={"Ta bort"} classname={"btn1"} func={removeFromFavourites} />)
