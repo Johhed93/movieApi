a3f95b2708f9aa4dee54499a80bd07affd634fdf
Johan hedlund Bergqvist <johan.bergqvist@gokstadakademiet.no>
2024-08-30 13:06:11 +0200

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,46 @@ import Navbar from "./components/Navbar";
 import ReadMore from "./pages/Readmore";
 const App = () => {
   const [movies, setNewMovies] = useState([]);
-  const[favourites, setNewFavourites]= useState([])
+  const [favourites, setNewFavourites] = useState([]);
   useEffect(() => {
-    fetchData({ setMovies: setNewMovies })
+    fetchData({ setMovies: setNewMovies });
   }, []);
 
   return (
     <>
-    
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home movies={movies} setNewFavourites={setNewFavourites} favourites={favourites}/>} />
-          <Route path="/Favourites" element={<Favourites favourites={favourites} setNewFavourites={setNewFavourites}/>}></Route>
-          <Route path="/MyPage" element={<MyPage/>}></Route>
-          <Route path="/Movies/:name" element={<ReadMore movies={movies}/>}/>
+          <Route
+            path="/"
+            element={
+              <Home
+                movies={movies}
+                setNewFavourites={setNewFavourites}
+                favourites={favourites}
+              />
+            }
+          />
+          <Route
+            path="/Favourites"
+            element={
+              <Favourites
+                favourites={favourites}
+                setNewFavourites={setNewFavourites}
+              />
+            }
+          ></Route>
+          <Route path="/MyPage" element={<MyPage />}></Route>
+          <Route
+            path="/Movies/:name"
+            element={
+              <ReadMore
+                movies={movies}
+                favourites={favourites}
+                setNewFavourites={setNewFavourites}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
