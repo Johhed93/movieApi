d633ffcc305794519d150f119d6f6d63cf54edf4
Johan hedlund Bergqvist <johan.bergqvist@gokstadakademiet.no>
2024-08-30 13:06:02 +0200

    started on readmore

--- src/pages/Readmore.jsx
+++ src/pages/Readmore.jsx
@@ -1,7 +1,18 @@
-const ReadMore = ()=>{
+import "./Favourites.css"
+import { useParams } from "react-router-dom";
+import ShowMore from "../components/showMore";
+const ReadMore = ({movies, favourites, setNewFavourites})=>{
+    const place= useParams();
+    const selectedMovie= movies.filter((movie)=>movie.name===place.name)
     return (
-        <>
-        </>
+        <main className="center-content flex">
+            <h1>Les mer om</h1>
+            <div>
+            {selectedMovie.map((movie, index)=>(
+            <ShowMore key={index} movie={movie} setNewFavourites={setNewFavourites} favourites={favourites}/>
+            ))}
+            </div>
+        </main>
     )
 }
 export default ReadMore
