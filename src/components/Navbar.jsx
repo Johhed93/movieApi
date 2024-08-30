ea039fe6ce37a953dcb15602cbb3f305555366e4
Johan hedlund Bergqvist <johan.bergqvist@gokstadakademiet.no>
2024-08-30 08:07:36 +0200

    fixed design and classes for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,42 @@
-import {Link} from "react-router-dom"
-const Navbar=()=>{
-    const links= [{
-    to:"/",
-    name:"Home"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+const Navbar = () => {
+  const links = [
+    {
+      to: "/",
+      name: "Home",
     },
     {
-        to:"/Favourites",
-        name:"Favoriter"
-    },{
-        to:"/MyPage",
-        name:"Min sida"
-    }
-]
-return(
-  <header>
-    <nav>
-        <div>Hejhej</div>
-        <ul>
-            {links.map((link, index)=>(
-                <li key={index}>
-                    <Link to={link.to}>{link.name}</Link>
-                </li>
-            ))}
+      to: "/Favourites",
+      name: "Favoriter",
+    },
+    {
+      to: "/MyPage",
+      name: "Min sida",
+    },
+  ];
+  return (
+    <header>
+      <nav>
+        <div className="logo-container">
+          <img
+            className="logo"
+            src="https://cdn.pixabay.com/photo/2015/12/09/22/24/filmklappe-1085692_1280.png"
+            alt="logo"
+          />
+          <span>Movie api</span>
+        </div>
+        <ul className="navigation-list">
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link className="navigation-link" to={link.to}>
+                {link.name}
+              </Link>
+            </li>
+          ))}
         </ul>
-    </nav>
-  </header>
-)
-}
-export default Navbar
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
