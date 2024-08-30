f3bded5127d83635ba05a57fb6194dfaf3ca04ad
Johan hedlund Bergqvist <johan.bergqvist@gokstadakademiet.no>
2024-08-30 10:50:06 +0200

    favourites page fixed

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,6 +1,30 @@
-const Favourites= ()=>{
+import "./Favourites.css"
+
+import Button from "../components/buttons/Button"
+import ShowMovie from "../components/ShowMovie"
+const Favourites= ({favourites, setNewFavourites })=>{
+
 return(
-    <h1>Favourites</h1>
+    <main className="center-content">
+    {favourites.length<0 ? (
+<div className="flex">
+        <h1>Du har inga favoriter en.</h1>
+        <Button classname={"btn1"} text={"Gå tillbaka"}/>
+        </div>
+    ): 
+    (
+        <div className="flex">
+        <h1>Mina favoriter</h1>
+        <div className="item-holder">
+        {favourites.map((movie, index)=>(
+        <ShowMovie key={index} movie={movie} setNewFavourites={setNewFavourites} favourites={favourites}/>
+        ))}
+        </div>
+        </div>
+    )
+
+    }
+    </main>
 )
 }
 export default Favourites
