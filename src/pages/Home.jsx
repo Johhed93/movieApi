87657c86fdafa34e1a262d973bc46f4493ec370b
Johhed93 <jhedlundbergqvist@gmail.com>
2024-09-02 21:26:43 +0200

    started with filtering movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import ShowMovie from "../components/ShowMovie";
 import "./Home.css"
+import { useEffect, useState } from "react";
 const Home = ({movies, favourites,setNewFavourites}) => {
- 
-  return( <div className="home-container">
-    {movies.map((movie, index)=>{
+  const [userSelection, setUserSelection]= useState([])
+  useEffect(()=>{
+  const sortByRating = movies.sort((a,b)=> b.rating- a.rating)
+  setUserSelection(sortByRating)
+  }, [])
+
+  return( 
+   <div className="home-container">
+  <div className="movie-holder">
+    {userSelection.map((movie, index)=>{
      {return <ShowMovie key={index} movie={movie} setNewFavourites={setNewFavourites} favourites={favourites}/>}
     })}
-  
+
   </div>
+  </div> 
   )
 };
 export default Home;
