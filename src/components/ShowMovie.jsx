b2913c0b802d56e7df2248e1e55690bb4884a07e
Johhed93 <jhedlundbergqvist@gmail.com>
2024-09-02 21:26:16 +0200

    added state for onError on image and handler

--- src/components/ShowMovie.jsx
+++ src/components/ShowMovie.jsx
@@ -2,7 +2,13 @@ import Button from "./buttons/Button";
 import { useState } from "react";
 import "./ShowMovie.css";
 import { Link, useParams } from "react-router-dom";
+import ImageOnError from "./utils/imageOnError";
 const ShowMovie = ({ movie, favourites,setNewFavourites }) => {
+  
+  const [imageError, setImageError]= useState(false);
+  const handleImageError= ()=>{
+    setImageError(true);
+  }
   const addToFavourites = () => {
     setNewFavourites((prevState) => [...prevState, movie]);
   };
@@ -10,6 +16,8 @@ const ShowMovie = ({ movie, favourites,setNewFavourites }) => {
     const updatedList= favourites.filter(favourite=> favourite.name !== movie.name)
     setNewFavourites(updatedList)
   }
+ 
+
 const location= useParams();
 
   const isInFavourites= favourites.some(favourite=>favourite.name===movie.name)
@@ -18,7 +26,9 @@ const location= useParams();
       <div className="bg-blur1"></div>
       <div className="bg-blur2"></div>
       <div className="bg-blur3"></div>
-      <img src={movie.thumb_url} className="thumb-nail"/>
+      {!imageError ? (<img src={movie.thumb_url} className="thumb-nail" onError={handleImageError}/>) 
+      : 
+      (<ImageOnError/>)}
       <div className="card">
         <h2>{movie.name}</h2>
         <p>Genre: {movie.genre.join(" ")}</p>
---
+++ src/components/utils/ImageOnError.jsx
@@ -0,0 +1,15 @@
+import "./ImageOnError.css";
+
+const ImageOnError= ()=>{
+    return (
+        <div className="on-error">
+         <div className="bg-style1"></div>
+         <div className="bg-style2"></div>
+         <div className="bg-style3"></div>
+         <p>
+            Ingen bild tillgänglig
+         </p>
+        </div>
+    )
+}
+export default ImageOnError
