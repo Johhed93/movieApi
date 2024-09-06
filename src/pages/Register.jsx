df7caed5323f668e7de93245e4ec46b3835b6cdb
Johan hedlund Bergqvist <johan.bergqvist@gokstadakademiet.no>
2024-09-06 13:58:34 +0200

    added ref to inout elements

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,39 @@
-import { useState, useRef } from "react"
-const Register = ()=>{
-    return(
-        <>
-        
-        </>
-    )
-}
-export default Register
+import { useState, useRef } from "react";
+import "./Register.css";
+const Register = () => {
+  const nameInput = useRef();
+  const lastNameInput = useRef("");
+  const usernameInput = useRef("");
+  const passwordInput = useRef("");
+  const [userRegister, setUserRegister] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!nameInput.current) return
+
+    console.log(nameInput.current.value);
+    nameInput.current.value=""
+  };
+  return (
+    <div className="content-container">
+      <div className="bg-image1"></div>
+      <div className="bg-image2"></div>
+      <div className="bg-image3"></div>
+      <h1>Registrera dig</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Førnavn:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          ref={nameInput}
+        />
+        <button className="btn1" type="submit">
+          Skicka
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Register;
